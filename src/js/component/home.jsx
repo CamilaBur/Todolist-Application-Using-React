f5c3df835c5d95a64eeb1a96b9ca04fe99ad035a
CamilaBur <burgueno.camila03@gmail.com>
2023-01-11 15:33:40 +0000

    mi primer commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,47 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
+import React, { useState } from "react";
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+  //declaracion de estados
+  // espacio de memoria, la funcion que actualiza elvalor inicial
+  const todos = [""]
 
+  //1. creamos un estado del input tareas, es lo que el usuario va a escribir porque es un string vacio, y luego se actualiza y se guarda en el array
+  const [tareas, setTareas] = useState(""); 
+  const [lista, setLista] = useState([]);
+
+  //3.vinculamos la funcion
+  //function handletareas(e) {setTareas(e.target.value); setLista(e.target.value);}
+
+  //4. procesamos todos los datos del formulario
+  function enviarDatos(e) {
+    e.preventDefault(); // detenemos el comportamiento predeterminado para procesar nuestro codigo
+    console.log(lista, tareas);
+  }
+  return (
+    <>
+      <form className="container" onSubmit={enviarDatos}>
+        {/* aca nuestra card */}
+        <div className="card">
+          <div className="card-body">
+            <input
+              type="text"
+              className="form-control"
+              aria-describedby="este es el input del boton"
+              onChange={(e) => {
+                setTareas(e.target.value);
+              }}
+              placeholder="enter your list item here ..."
+            />
+          </div>
+        </div>
+        {/* aca nuestro boton */}
+        <button type="submit" className="btn btn-primary" onChange={(e) => {
+                setLista(e.target.value);
+              }} onClick={enviarDatos}>
+          Agregar tarea
+        </button>
+      </form>
+    </>
+  );
+};
 export default Home;
