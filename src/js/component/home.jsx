7de2492b7a35e38c83d46a20d1b04a9f2ca9bbef
CamilaBur <burgueno.camila03@gmail.com>
2023-01-11 20:55:34 +0000

    Mi second commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,47 +1,64 @@
-import React, { useState } from "react";
-//create your first component
+import React, {useState} from "react";
+// create your first component
 const Home = () => {
-  //declaracion de estados
-  // espacio de memoria, la funcion que actualiza elvalor inicial
-  const todos = [""]
 
-  //1. creamos un estado del input tareas, es lo que el usuario va a escribir porque es un string vacio, y luego se actualiza y se guarda en el array
-  const [tareas, setTareas] = useState(""); 
-  const [lista, setLista] = useState([]);
 
-  //3.vinculamos la funcion
-  //function handletareas(e) {setTareas(e.target.value); setLista(e.target.value);}
+    const [tarea, setTarea] = useState("");
+    const [lista, setLista] = useState([]);
 
-  //4. procesamos todos los datos del formulario
-  function enviarDatos(e) {
-    e.preventDefault(); // detenemos el comportamiento predeterminado para procesar nuestro codigo
-    console.log(lista, tareas);
-  }
-  return (
-    <>
-      <form className="container" onSubmit={enviarDatos}>
-        {/* aca nuestra card */}
-        <div className="card">
-          <div className="card-body">
-            <input
-              type="text"
-              className="form-control"
-              aria-describedby="este es el input del boton"
-              onChange={(e) => {
-                setTareas(e.target.value);
-              }}
-              placeholder="enter your list item here ..."
-            />
-          </div>
-        </div>
-        {/* aca nuestro boton */}
-        <button type="submit" className="btn btn-primary" onChange={(e) => {
-                setLista(e.target.value);
-              }} onClick={enviarDatos}>
-          Agregar tarea
-        </button>
-      </form>
-    </>
-  );
+
+    const DeleteItems = (indexItem) => {
+      setLista((prevState) =>
+        prevState.filter((listItems, index) => index !== indexItem)
+      );
+      };
+
+    function enviarDatos(e) {
+        e.preventDefault();
+        setLista([
+            ...lista,
+            tarea])
+            setTarea("");
+    }
+    console.log(lista);
+    return (
+        <>
+            <div className="container">
+               
+                <div className="card">
+                    <div className="card-body">
+                        <input type="text" className="input m-1 w-75" value={tarea} id="exampleInput" aria-describedby="inputHelp"
+                            onChange={
+                                (e) => {
+                                    setTarea(e.target.value);
+                                }
+                            }
+                            placeholder="No hay tareas, añadir tareas"/>
+                        <button type="submit" className="btn btn-primary btn-sm"
+                            onChange={
+                                (e) => {
+                                    setLista(e.target.value);
+                                }
+                            }
+                            onClick={enviarDatos}>
+                            Agregar tarea
+                        </button>
+                        
+                    </div>
+                    <div className="to-do-list d-flex">
+			  <ul>{lista.map((item, index) => (
+        <li key={index}>
+          {item}
+          <button className="btn" onClick={() => DeleteItems(index)}>
+            <i className="fas fa-trash-alt" />
+          </button>
+        </li>
+      ))}</ul>
+			</div>
+                </div>
+
+            </div>
+        </>
+    );
 };
 export default Home;
